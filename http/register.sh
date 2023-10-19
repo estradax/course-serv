f8ca66f36a4781103d677f6ea8517d51345ff5cb
estradax <azharumuhammad@gmail.com>
2023-10-19 19:20:27 +0700

    add argument to http

--- http/register.sh
+++ http/register.sh
@@ -1,5 +1,34 @@
+set -x
+
+while test $# -gt 0; do
+  case "$1" in
+    -name)
+		shift
+		name=$1
+		shift
+		;;
+	-email)
+		shift
+		email=$1
+		shift
+		;;
+	-password)
+		shift
+		password=$1
+		shift
+		;;
+	*)
+      	break
+      	;;
+  esac
+done
+
+name=${name:-JohnDoe}
+email=${email:-johndoe@mail.com}
+password=${password:-foobar1234}
+
 curl -v -X POST \
   -H 'Connection: close' \
   -H 'Content-Type: application/json' \
-  -d '{"name": "John Doe", "email": "johndoe5@mail.com", "password": "foobar1234"}' \
+  -d '{"name": "'$name'", "email": "'$email'", "password": "'$password'"}' \
   http://localhost:8080/api/v1/register
