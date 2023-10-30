46e759d4bf7e8b035d4f0fc0632b366c1ec8ddb9
estradax <azharumuhammad@gmail.com>
2023-10-30 21:39:41 +0700

    fix

--- cmd/course-serv/course-serv.go
+++ cmd/course-serv/course-serv.go
@@ -33,7 +33,7 @@ func main() {
 	app.Post("/api/v1/register", h.Register)
 	app.Post("/api/v1/login", h.Login)
 
-	app.Get("/api/v1/courses", h.Authenticated, h.CourseGetAll)
+	app.Get("/api/v1/courses", h.CourseGetAll)
 
 	if err := app.Listen(":8080"); err != nil {
 		log.Fatalln("Cannot listen: ", err.Error())
